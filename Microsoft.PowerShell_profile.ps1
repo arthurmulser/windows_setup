7d709e991fe80e2d6093ecb3a30fd1672bc8806e
Arthur Mulser <arthur.mulser@hotmail.com>
2025-10-21 09:52:37 -0300

    20251021 - new: laravel;

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
+### ### ### docker;
+
 function du3 { # ok;
     Write-Host "docker compose up -d;"
     docker compose up -d
+    Write-Host "ok;"
 }
 
 function helpd3 {
@@ -9,11 +12,12 @@ function helpd3 {
     Write-Host "--- --- --- --- ---"
 }
 
-### ### ###
+### ### ### git;
 
 function ga3 { # ok;
     Write-Host "executando: git add .;"
     git add .
+    Write-Host "ok;"
 }
 
 function gc3 { # ok;
@@ -24,11 +28,13 @@ function gc3 { # ok;
     $date = Get-Date -Format "yyyyMMdd"
     Write-Host "executando: git commit -m standard;"
     git commit -m "$date - $message;"
+    Write-Host "ok;"
 }
 
 function gp3 { # ok;
     Write-Host "executando: git push origin main;"
     git push origin main
+    Write-Host "ok;"
 }
 
 function helpg3 {
@@ -39,26 +45,30 @@ function helpg3 {
     Write-Host "--- --- --- --- ---"
 }
 
-### ### ###
+### ### ### flutter;
 
 function fb3 { # ok;
     Write-Host "executando: flutter build apk;"
-    flutter build apk  
+    flutter build apk
+    Write-Host "ok;"
 }
 
 function fc3 { # ok;
     Write-Host "executando: flutter clean;"
-    flutter clean 
+    flutter clean
+    Write-Host "ok;"
 }
 
 function fd3 { # ok;
     Write-Host "executando: flutter devices;"
     flutter devices
+    Write-Host "ok;"
 }
 
 function fr3 { # ok;
     Write-Host "executando: flutter run;"
-    flutter run   
+    flutter run 
+    Write-Host "ok;"  
 }
 
 function helpf3 {
@@ -70,7 +80,18 @@ function helpf3 {
     Write-Host "--- --- --- --- ---"
 }
 
-### ### ###
+### ### ### laravel;
+
+function lc3 { # ok;
+    Write-Host "executando: php artisan config:clear, php artisan route:clear, php artisan view:clear, php artisan optimize:clear;"
+    php artisan config:clear
+    php artisan route:clear
+    php artisan view:clear
+    php artisan optimize:clear 
+    Write-Host "ok;"
+}
+
+### ### ### code;
 
 function helpc3 {
     Write-Host "code:"
@@ -82,4 +103,5 @@ function helpc3 {
 function p3 {
     Write-Host ". cifraoPROFILE;"
     . $PROFILE
+    Write-Host "ok;"
 }
