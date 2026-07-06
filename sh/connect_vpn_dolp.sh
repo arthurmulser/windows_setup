2b6b3fa2b77a1729ea9c31452b70d607cf52177c
arthurmulser <arthurmulser@hotmail.com>
2026-07-06 18:13:02 -0300

    20260706 - new: sh/connect_vpn_dolp.sh;

---
+++ sh/connect_vpn_dolp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ==============================================================================;
+# como usar:;
+# 1. garanta permissão de execução (uma única vez): chmod +x connect_vpn_dolp.sh;
+
+# 2. execute: sudo ./connect_vpn_dolp.sh;
+# ==============================================================================;
+
+sudo openfortivpn \
+  177.107.40.218:20443 \
+  -u ti.dolp.arthur \
+  --trusted-cert 35a43b39cf5e5704411b536192dbe4a32be911dd6659e8ecc6c75ec98d002f2d
+
