a156c1f7ad57ae35d76c46f1cae8ea03eb98bed6
arthurmulser <arthurmulser@hotmail.com>
2026-07-13 21:41:40 -0300

    20260713 - update: sql/sgd_almoxarifado.sql;

--- sql/sgd_almoxarifado.sql
+++ sql/sgd_almoxarifado.sql
@@ -17,4 +17,24 @@ where idtb_produtos_enel_rastro = 33594;
 select * from tb_produtos_enel_manifesto_itens 
 where idtb_produtos_enel_manifesto_itens in ('765025', '765026', '951542', '979460');
 
-select * from tb_oper_pessoa where nome like '%ODAIR FERNANDES FRIAS%';
+select * from tb_oper_pessoa 
+where nome like '%MARCOS ANTONIO DA SILVA SEVERO%'
+or nome like '%THALLES EDUARDO DE ANDRADE%'
+or nome like '%RICHARD ANTHONY PEREIRA%'
+or nome like '%CLAUDIO HENRICH DE OLIVEIRA ALVES%'
+or nome like '%ALMIR JOSE ALVES DA SILVA SOBRINHO%'
+or nome like '%DINIWEY LOPES DA SILVA%'
+or nome like '%WENDERSON PEREIRA DA SILVA%'
+or nome like '%MARCIO LAGOS DE CARVALHO%'
+;
+
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
