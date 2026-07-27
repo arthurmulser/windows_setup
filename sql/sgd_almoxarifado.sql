9afcadd9442e8269bcf75d0f753382e2f8404d7c
arthurmulser <arthurmulser@hotmail.com>
2026-07-27 10:53:58 -0300

    20260727 - update: sql/sgd_almoxarifado.sql;

--- sql/sgd_almoxarifado.sql
+++ sql/sgd_almoxarifado.sql
@@ -35,9 +35,24 @@ or nome like '%MARCIO LAGOS DE CARVALHO%'
 ;
 
 UPDATE tb_frota_veiculo SET status = 1 
-WHERE (idtb_Veiculo = 603); ## atualizar status da frota
-;
+WHERE (idtb_Veiculo = 603)
+; -- atualizar status da frota;
 
+
+UPDATE tb_produtos_enel_rastro AS a
+        INNER JOIN
+    tb_estoque_entidade AS b ON b.idtb_estoque_entidade = a.idtb_estoque_entidade
+        INNER JOIN
+    tb_oper_pessoa AS c ON c.idtb_oper_pessoa = b.id_estrangeiro
+        INNER JOIN
+    tb_produtos_enel AS d ON d.idtb_produtos_enel = a.idtb_produtos_enel 
+SET 
+    a.idtb_estoque_entidade = 519
+WHERE
+    a.esta_em_mov NOT IN (1) AND b.tipo = 4
+        AND c.status = 2
+        AND c.idtb_oper_base NOT IN (8 , 10, 11)
+; -- colocar rastros no saldo da rota;
 
  
 
