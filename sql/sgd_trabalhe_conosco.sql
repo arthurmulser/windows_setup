6cc7794a25e32acb8b20a2420921a980adaef8d8
arthurmulser <arthurmulser@hotmail.com>
2026-08-03 19:58:19 -0300

    20260803 - update: sql/sgd_trabalhe_conosco.sql;

--- sql/sgd_trabalhe_conosco.sql
+++ sql/sgd_trabalhe_conosco.sql
@@ -1,1 +1,7 @@
 -- blank_form21_site_descricao_vaga; // tela onde aparece o card da vaga com cargo, descrição, requisitos e benefícios;
+
+-- blank_abertura_vaga_opcoes; // abre leque de opções para uma vaga;
+
+-- blank_abertura_vaga_finalizar; // para cancelar/finalizar (concluir) uma vaga;
+
+
